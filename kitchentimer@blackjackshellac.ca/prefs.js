32a60608829e07de52a5d718a2c1fa3376737ca9
Steeve McCauley <steeve@oneguycoding.com>
2021-03-30 14:45:37 -0400

    Copied from settings.ui
    * needs a lot of work
    
    Issue #57

--- kitchentimer@blackjackshellac.ca/prefs.js
+++ kitchentimer@blackjackshellac.ca/prefs.js
@@ -16,7 +16,7 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-const { Gio, Gtk, GLib } = imports.gi;
+const { GObject, Gio, Gtk, GLib } = imports.gi;
 const ByteArray = imports.byteArray;
 
 const GETTEXT_DOMAIN = 'kitchen-timer-blackjackshellac';
@@ -49,13 +49,88 @@ const KitchenTimerPrefs = GObject.registerClass({
     _init(params = {}) {
         super._init(params);
 
-        this._settings = new Settings();
+        this.settings = new Settings();
         this.logger = new Logger('kt_prefs', this._settings);
     }
-    
+
     _onButtonClicked(button) {
-        button.set_label('Clicked!');
+      let msg=this.logger.debug('Clicked at %s', new Date().toLocaleTimeString());
+      button.set_label(msg);
     }
+
+    get settings() {
+      return this._settings;
+    }
+
+    set settings(val) {
+      this._settings = val;
+    }
+
+    // /**
+    //  * Get Gtk Builder object by id
+    //  */
+    // _bo(id) {
+    //   return this._builder.get_object(id);
+    // }
+
+    // /**
+    //  * Bind setting to builder object
+    //  */
+    // _ssb(key, object, property, flags=Gio.SettingsBindFlags.DEFAULT) {
+    //   if (object) {
+    //     this.settings.bind(key, object, property, flags);
+    //   } else {
+    //     this.logger.error("object is null for key=%s", key);
+    //   }
+    // }
+
+    // _bo_ssb(id, property, flags=Gio.SettingsBindFlags.DEFAULT) {
+    //   let object = this._bo(id);
+    //   let key=id.replace(/_/g, '-');
+    //   this._ssb(key, object, property, flags);
+    // }
+
+    // _bind() {
+    //   this._bo_ssb('accel_enable', 'active');
+
+    //   this._bo_ssb('notification', 'active');
+    //   this._bo_ssb('notification_sticky', 'active');
+
+    //   let show_time = this._bo('show_time');
+    //   this._ssb('show-time', show_time, 'active');
+
+    //   let show_progress = this._bo('show_progress');
+    //   this._ssb('show-progress', show_progress, 'active');
+
+    //   let show_label = this._bo('show_label');
+    //   this._ssb('show-label', show_label, 'active');
+
+    //   let play_sound = this._bo('play_sound');
+    //   this._ssb('play-sound', play_sound, 'active');
+    //   this._ssb('play-sound', this._bo('play_sound2'), 'active');
+
+    //   let sound_loops = this._bo('sound_loops');
+    //   this._ssb('sound-loops', sound_loops, 'value');
+
+    //   let sort_by_duration = this._bo('sort_by_duration');
+    //   this._ssb('sort-by-duration', sort_by_duration, 'active');
+
+    //   let sort_descending = this._bo('sort_descending');
+    //   this._ssb('sort-descending', sort_descending, 'active');
+
+    //   let save_quick_timers = this._bo('save_quick_timers')
+    //   this._ssb('save-quick-timers', save_quick_timers, 'active');
+
+    //   let detect_dupes = this._bo('detect_dupes');
+    //   this._ssb('detect-dupes', detect_dupes, 'active');
+
+    //   let volume_level_warn = this._bo('volume_level_warn');
+    //   this._ssb('volume-level-warn', volume_level_warn, 'active');
+
+    //   let volume_threshold = this._bo('volume_threshold');
+    //   this._ssb('volume-threshold', volume_threshold, 'value');
+
+    // }
 });
 
 function init() {
@@ -77,5 +152,4 @@ function buildPrefsWidget() {
 
   return widget;
 }
-
 
